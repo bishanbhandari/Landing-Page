8018292c7437553fc367b2f8a5565181354ecba5
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-06-14 13:59:14 +0545

    changes

--- src/Components/Backgroud/Background.jsx
+++ src/Components/Backgroud/Background.jsx
@@ -4,6 +4,8 @@ import video1 from '../../assets/video1.mp4';
 import image1 from '../../assets/image1.png';
 import image2 from '../../assets/image2.png';
 import image3 from '../../assets/image3.png';
+
+
 const Background = ({playStatus,herocount}) => {
   
     if(playStatus){
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Hero.css';
 import arrow_btn from '../../assets/arrow_btn.png';
 import play_icon from '../../assets/play_icon.png';
+import pause_icon from '../../assets/pause_icon.png';
+
+
 const Hero = ({heroData,setHeroCount,herocount,setPlayStatus,playStatus}) => {
   return (
     <div className='hero'>
@@ -20,8 +23,11 @@ const Hero = ({heroData,setHeroCount,herocount,setPlayStatus,playStatus}) => {
             <li  onClick={()=>setHeroCount(0)} className={herocount===0 ? "hero-dot orange":"hero-dot"}></li>
             <li  onClick={()=>setHeroCount(1)} className={herocount===1 ? "hero-dot orange":"hero-dot"}></li>
             <li  onClick={()=>setHeroCount(2)} className={herocount===2 ? "hero-dot orange":"hero-dot"}></li>
-
           </ul>
+          <div className="hero-play">
+            <img  onClick={()=>setPlayStatus(!playStatus)} src={playStatus ? pause_icon:play_icon} alt="" />
+            <p>See the video</p>
+          </div>
       </div>
     </div>
   )
